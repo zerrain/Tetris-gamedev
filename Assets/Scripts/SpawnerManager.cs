e62deed92e97e97ab35307ae9d11aa45eb827844
zaid arain <zaid34108@hotmail.com>
2019-10-14 05:19:31 +1100

    Mass functionality added to Recreated Game Scene
    -Score counter added
    -All tetris game mechanics added
    -Initial pause screen setup
    -Edits to block prefabs to accomodate for game functionality
    -Minor visual edits to all game scenes

---
+++ Assets/Scripts/SpawnerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+    public GameObject[] tetrisBlocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnNewBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnNewBlock()
+    {
+        Instantiate(tetrisBlocks[Random.Range(0, tetrisBlocks.Length)], transform.position, Quaternion.identity);
+    }
+}
