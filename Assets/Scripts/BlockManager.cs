9e3a6d1ea25d8de9bbb648b5bd357f384bf59b06
zaid arain <zaid34108@hotmail.com>
2019-10-14 07:27:36 +1100

    Code comments
    Added code comments for all script files

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -6,16 +6,16 @@ using UnityEngine.UI;
 
 public class BlockManager : MonoBehaviour
 {
-    private float prevTime;
-    private float timeBetweenDrops = 0.3f;
-    public static int width = 10;
+    private float prevTime; //Variable to store the last time in seconds the block was moved down
+    private float timeBetweenDrops = 0.3f; //Interval block moves down in seconds
+    public static int width = 10; 
     public static int height = 20;
     public Vector3 rotationPoint;
-    private static Transform[,] tetrisBoard = new Transform[width, height];
+    private static Transform[,] tetrisBoard = new Transform[width, height]; //2D array for handling the tetris board
     public int score = 0;
     public Text scoreText;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update, called upon the creation of a new block, if the new block placement is invalid, it is gameover
     void Start()
     {
         if (!isValidMove())
@@ -26,28 +26,32 @@ public class BlockManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Moves the block left if the left arrow key is pressed/held
         if (Input.GetKeyDown(KeyCode.LeftArrow) || (Time.frameCount % 20 == 0 && Input.GetKey(KeyCode.LeftArrow))) 
         {
             transform.position += new Vector3(-1, 0, 0);
             if (!isValidMove())
                 transform.position -= new Vector3(-1, 0, 0);
         }
+        // Moves the block right if the right arrow key is pressed/held
         else if (Input.GetKeyDown(KeyCode.RightArrow) || (Time.frameCount % 20 == 0 && Input.GetKey(KeyCode.RightArrow)))
         {
             transform.position += new Vector3(1, 0, 0);
             if (!isValidMove())
                 transform.position -= new Vector3(1, 0, 0);
         }
+        // Rotates the block if the up arrow key is pressed
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
             if (!isValidMove())
                 transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
         }
-
+        // Moves the block down faster if the down arrow key is pressed/held
         if (Time.time - prevTime > (Input.GetKey(KeyCode.DownArrow) ? timeBetweenDrops / 8 : timeBetweenDrops))
         {
             transform.position += new Vector3(0, -1, 0);
+            // If the block is under to move down anymore, it is added to the grid as a placed block and the function to spawn a new block is called
             if (!isValidMove())
             {
                 transform.position -= new Vector3(0, -1, 0);
@@ -60,16 +64,20 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // Function to check if the current move is a valid move
     bool isValidMove()
     {
+        // Determines the x and y positions of each square in the current tetris block
         foreach (Transform child in transform)
         {
             int x = Mathf.RoundToInt(child.transform.position.x);
             int y = Mathf.RoundToInt(child.transform.position.y);
 
+            // If the block would be moved to an invalid location outside of the tetris board
             if (x < 0 || x >= width || y < 0 || y >= height)
                 return false;
 
+            // If the block would be moved to a location where a block already exists
             if (tetrisBoard[x, y] != null)
                 return false;
         }
@@ -77,6 +85,7 @@ public class BlockManager : MonoBehaviour
         return true;
     }
 
+    // Function responsible for adding the block to the tetris board by adding each square from the tetris block one by one 
     void addBlockToBoard()
     {
         foreach (Transform child in transform)
@@ -88,19 +97,24 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // Checks for any complete lines
     void completeLinesCheck()
     {
         for (int i = height - 1; i >= 0; i--)
         {
+            // If a complete line exists
             if (lineExists(i))
             {
+                // Clear the line, move blocks down, add one to the score, add one to the score streak if the new featured mode
                 clearLine(i);
                 moveBlocksDown(i);
                 score++;
                 ScoreManager.score++;
+                // If the current game mode is the new featured mode
                 if (SceneManager.GetActiveScene().buildIndex == 2)
                 {
                     ScoreStreakManager.scoreStreak++;
+                    //If the scorestreak is achieved, clear the bottom most line after clearing the completed line and reset the scorestreak
                     if (ScoreStreakManager.scoreStreak == 5)
                     {
                         ScoreStreakManager.scoreStreak = 0;
@@ -112,6 +126,7 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // Checks if a completed line exists
     bool lineExists(int i)
     {
         for (int j = 0; j < width; j++)
@@ -123,6 +138,7 @@ public class BlockManager : MonoBehaviour
         return true;
     }
 
+    // Clears the line
     void clearLine(int i)
     {
         for (int j = 0; j < width; j++)
@@ -131,7 +147,8 @@ public class BlockManager : MonoBehaviour
             tetrisBoard[j, i] = null;
         }
     }
-
+    
+    // Function responsible for moving the blocks down after a line is cleared
     void moveBlocksDown(int i)
     {
         for (int j = i; j < height; j++)
@@ -148,6 +165,7 @@ public class BlockManager : MonoBehaviour
         }
     }
 
+    // Function called when the game is over, displays game over menu and resets variables for a new game
     void gameOver()
     {
         ScoreManager.score = 0;
--- Assets/Scripts/GameOverMenuManager.cs
+++ Assets/Scripts/GameOverMenuManager.cs
@@ -5,11 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GameOverMenuManager : MonoBehaviour
 {
-    public GameObject[] gameOverObjects;
+    public GameObject[] gameOverObjects; //Contains game over objects such as game over menu buttons and title text
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initializes game over objects
         gameOverObjects = GameObject.FindGameObjectsWithTag("showOnGameOver");
         hideGameOverObjects();
     }
@@ -20,27 +21,32 @@ public class GameOverMenuManager : MonoBehaviour
 
     }
 
+    // Loads the current scene again
     public void restartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    // Loads the main menu scene
     public void launchMainMenu()
     {
         SceneManager.LoadScene("MainMenuScene");
     }
 
+    // Exits the game
     public void exitGame()
     {
         UnityEditor.EditorApplication.isPlaying = false;
     }
 
+    // Hides all game over objects
     public void hideGameOverObjects()
     {
         foreach (GameObject gameObject in gameOverObjects)
             gameObject.SetActive(false);
     }
 
+    // Displays all game over objects
     public void showGameOverObjects()
     {
         foreach (GameObject gameObject in gameOverObjects)
